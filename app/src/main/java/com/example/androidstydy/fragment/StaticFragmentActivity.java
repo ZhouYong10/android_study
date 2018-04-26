d07db4db7477370f3757c0604bf67bfaf81d582e
zhouyong10 <1059981087@qq.com>
2018-04-26 21:58:47 +0800

    添加静态fragment示例

---
+++ app/src/main/java/com/example/androidstydy/fragment/StaticFragmentActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidstydy.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidstydy.R;
+
+public class StaticFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_fragment);
+    }
+}
